316d12b760b034deb1095ac57434fb34e74ac1dc
MrGeet <aldarbatomunkuev@gmail.com>
2021-05-04 09:47:50 +1200

    frontend setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import { useState, useMemo } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import SearchScreen from './Screens/SearchScreen';
 import HomeScreen from './Screens/HomeScreen';
 import ProfileScreen from './Screens/ProfileScreen';
 import Header from './components/Header';
@@ -9,8 +8,6 @@ import { Container } from 'react-bootstrap';
 import LoginScreen from './Screens/LoginScreen';
 import { UserContext } from './UserContext';
 import RegisterScreen from './Screens/RegisterScreen';
-import LikedByScreen from './Screens/LikedByScreen';
-import MatchesScreen from './Screens/MatchesScreen';
 
 function App() {
 	const userInfo = localStorage.getItem('userInfo')
@@ -29,9 +26,6 @@ function App() {
 					<Container>
 						<Route exact path="/" component={HomeScreen} />
 
-						<Route exact path="/search" component={SearchScreen} />
-						<Route exact path="/likedBy" component={LikedByScreen} />
-						<Route exact path="/matches" component={MatchesScreen} />
 						<Route exact path="/login" component={LoginScreen} />
 						<Route exact path="/register" component={RegisterScreen} />
 						<Route exact path="/profile/:id" component={ProfileScreen} />
