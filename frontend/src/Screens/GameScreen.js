b96affb657eedb8c31e148743d1d5c3601597105
MrGeet <aldarbatomunkuev@gmail.com>
2021-05-04 23:30:53 +1200

    remove cards, render

--- frontend/src/Screens/GameScreen.js
+++ frontend/src/Screens/GameScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { pokerPlayers, mainPlayer as mainPlayerSelf } from '../data';
 import { Card, Button, Row, Col, FormControl, InputGroup, Modal } from 'react-bootstrap';
 import { socket } from '../socket';
+import { v4 as uuidv4 } from 'uuid';
 
 const backgroundStyle = {
 	background: 'url("../img/table.jpg")',
@@ -25,34 +26,57 @@ const activeCard = {
 const textStyle = {
 	fontSize: '0.8rem',
 };
+
+const getCardImgUrl = (card) => {
+	return `../img/cards/${card?.rank}${card?.suit.charAt(0).toUpperCase()}.svg`;
+};
+
 const GameScreen = ({ history, match }) => {
 	const gameId = match.params.gameId;
 	const [players, setPlayers] = useState([]);
 	const [amount, setAmount] = useState(0);
-	const [table, setTable] = useState({});
 	const [mainPlayer, setMainPlayer] = useState(mainPlayerSelf);
+	const [currentPlayer, setCurrentPlayer] = useState(null);
 	const [modal, setModal] = useState(false);
+	const [tableCards, setTableCards] = useState([]);
+	const [activePlayer, setActivePlayer] = useState(null);
 	// const data = socket.on('tableData');
 
 	useEffect(() => {
 		// if (gameId) {
 		// 	alert('game id is ' + gameId);
 		// }
-		setPlayers(pokerPlayers);
+		// setPlayers(pokerPlayers);
 		socket.on('currentSeat', (data) => {
 			if (data.currentSeat === undefined) {
 				history.push('/profile');
+			} else {
+				console.log(data);
+				setCurrentPlayer(data.currentSeat);
 			}
 		});
 
+		console.log('Current player is ' + currentPlayer);
 		socket.on('tableData', (data) => {
 			console.log(data);
 			const numOfSeats = data.seats.filter((el) => el !== null);
-
 			if (numOfSeats.length <= 1) {
 				setModal(true);
 			} else {
 				setModal(false);
+			}
+
+			//if game started
+			if (data.cards) {
+				const mappedPlayers = [];
+
+				for (var i = 0; i < numOfSeats.length; i++) {
+					mappedPlayers.push({ ...data?.seats[i], ...data?.cards[i] });
+				}
+				console.log(mappedPlayers);
+				setPlayers(mappedPlayers);
+				setTableCards(data.community);
+				setActivePlayer(data.active);
 			}
 		});
 
@@ -60,7 +84,7 @@ const GameScreen = ({ history, match }) => {
 			console.log('call something');
 			socket.emit('sitTable', { chips: mainPlayer.numberOfChips });
 		}
-	}, [mainPlayer]);
+	}, [mainPlayer, history]);
 
 	const handleLeave = () => {
 		socket.disconnect();
@@ -85,27 +109,37 @@ const GameScreen = ({ history, match }) => {
 				</Modal.Body>
 			</Modal>
 			<Row className="d-flex mt-auto justify-content-around fixed-bottom">
-				{players.map((player) => (
-					<div key={player.name}>
+				{players.map((player, index) => (
+					<div key={index}>
 						<div className="d-flex justify-content-center">
 							<img
 								className="playingCard"
 								alt="card"
-								src="../img/cards/BLUE_BACK.svg"
+								// src="../img/cards/BLUE_BACK.svg"
+								src={
+									currentPlayer === index
+										? getCardImgUrl(player.first)
+										: '../img/cards/BLUE_BACK.svg'
+								}
 							/>
 							<img
 								className="playingCard"
 								alt="card"
-								src="../img/cards/BLUE_BACK.svg"
+								// src="../img/cards/BLUE_BACK.svg"
+								src={
+									currentPlayer === index
+										? getCardImgUrl(player.second)
+										: '../img/cards/BLUE_BACK.svg'
+								}
 							/>
 						</div>
-						<Card style={player.isActive ? activeCard : { width: '12rem' }}>
+						<Card style={activePlayer === index ? activeCard : { width: '12rem' }}>
 							<Card.Img style={imgStyle} variant="top" src="../img/dices.png" />
 							<Card.Body>
-								<Card.Title style={textStyle}>Name: {player.name}</Card.Title>
-								<Card.Text style={textStyle}>
-									Number of chips: {player.numberOfChips}
-								</Card.Text>
+								{/* CHANGE INDEX TO NAME */}
+								<Card.Title style={textStyle}>Name: {index}</Card.Title>
+								<Card.Text style={textStyle}>Stack: {player.stackSize}</Card.Text>
+								<Card.Text style={textStyle}>Bet: {player.betSize}</Card.Text>
 							</Card.Body>
 						</Card>
 					</div>
@@ -152,11 +186,14 @@ const GameScreen = ({ history, match }) => {
 						<img className="playingCard" alt="card" src="../img/cards/BLUE_BACK.svg" />
 
 						<div className="d-flex justify-content-around w-75">
-							<img className="playingCard" alt="card" src="../img/cards/KS.svg" />
-							<img className="playingCard" alt="card" src="../img/cards/KS.svg" />
-							<img className="playingCard" alt="card" src="../img/cards/KS.svg" />
-							<img className="playingCard" alt="card" src="../img/cards/KS.svg" />
-							<img className="playingCard" alt="card" src="../img/cards/KS.svg" />
+							{tableCards.map((card, index) => (
+								<img
+									key={index}
+									className="playingCard"
+									alt="card"
+									src={getCardImgUrl(card)}
+								/>
+							))}
 						</div>
 					</div>
 				</Col>
